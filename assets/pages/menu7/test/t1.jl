e94b0243b66b3e8b989b5a982a5fadfbc1e5f78a
Tamás Cserteg <csertegt@gmail.com>
2019-12-24 00:30:43 +0100

    render with plotlyjs

---
+++ assets/pages/menu7/test/t1.jl
@@ -0,0 +1,10 @@
+# This file was generated, do not modify it. # hide
+using PlotlyJS, DelimitedFiles, Statistics, ORCA
+fp = joinpath("assets", "temperature", "temps.tsv")
+d, h = readdlm(abspath(fp), '\t', header = true)
+sc = scatter(;x=d[:,1], y=d[:,2], mode="markers")
+p = plot(sc)
+savefig(p, joinpath(@__DIR__, "plotly1.html"), format="html")
+savefig(p, joinpath(@__DIR__, "plotly1.png"), format="png")
+savefig(p, joinpath(@__DIR__, "output", "t2.out"), format="html")
+println("The average is:", mean(d[:,2]))
---
+++ assets/pages/menu7/test/t4.jl
@@ -0,0 +1,4 @@
+# This file was generated, do not modify it. # hide
+#hideall
+bp = joinpath(@__DIR__, "output")
+cp(joinpath(bp, "t2.out"),joinpath(bp, "t1.out"),force=true)
